12f18ad613f1fc376e8d18c0c703edad8b88238a
ibnemuhammad <ibnemuhammad50@gmail.com>
2024-02-19 23:04:32 +0500

    Changes Test output style

--- StringCleaner.js
+++ StringCleaner.js
@@ -2,8 +2,8 @@ function CleanString(str) {
         if (str && str.length > 0) {
             var StrippedString = str.replace(/(<([^>]+)>)/ig, "");
             StrippedString = StrippedString.trim();
-            StrippedString = StrippedString.replace(/&#160;/ig, " ");
             StrippedString = StrippedString.replace(/&nbsp;/ig, " ");
+            StrippedString = StrippedString.replace(/&#160;/ig, " ");
             if (StrippedString.length > 1000)
                 return StrippedString.slice(1, 1000);
             return StrippedString;
@@ -13,9 +13,12 @@ function CleanString(str) {
 
 console.log('Start');
 // run tests with expected result and input values
+TestStrippedString('', '');
+TestStrippedString(null, null);
 TestStrippedString('test', 'test');
 TestStrippedString('<h1>test</h1>', 'test');
 TestStrippedString('test&nbsp;1', 'test 1');
+TestStrippedString('test&#160;1', 'test 1');
 
 console.log('End');
 
@@ -24,8 +27,8 @@ console.log('End');
 function TestStrippedString(input, expected) {
     var result = CleanString(input);
     if (result === expected) {
-        console.log("Test passed for input ", input, " expected ", expected, " and result ", result);
+        console.log("Passed Given: ", input, " CleanString should return: ", expected);
     } else {
-        console.log("Test failed for input ", input, " expected ", expected, " and result ", result);
+        console.log("Failed Given: ", input, " CleanString should return: ", expected, " but returned: ", result);
     }
 }
